36b485f4130e1e8470e7c4622ca4f89a4878dfe2
fhood <fhood@FAHDAH>
2019-02-25 23:30:30 +0300

    more

---
+++ WebApplication3/Controllers/TmdbApiController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace demo.MVC.Controllers
+{
+    public class TmdbApiController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult GetFavoriteMovies()
+        {
+            return View();
+        }
+      
+
+        public ActionResult FindMovies() 
+        {
+            return View("FindMovies");
+        }
+
+        public ActionResult FindStars() 
+        {
+            return View("FindStars");
+        }
+
+        public ActionResult FindLatestMovies()
+        {
+            return View("FindLatestMovies");
+        }
+
+        public ActionResult FindMovieCredits()
+        {
+            return View("FindMovieCredits");
+        }
+    }
+}
---
+++ WebApplication3Tests/Controllers/TmdbApiControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using demo.MVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace demo.MVC.Controllers.Tests
+{
+    [TestClass()]
+    public class TmdbApiControllerTests
+    {
+        //----- test latest movies action returns the correct view
+        [TestMethod()]
+        public void FindLatestMoviesTest()
+        {
+            var controller = new TmdbApiController();
+            var result = controller.FindLatestMovies() as ViewResult;
+            Assert.AreEqual("FindLatestMovies", result.ViewName);
+
+        }
+
+        //------- Test Find Movies-------
+        [TestMethod()]
+        public void FindMovies()
+        {
+            var controller = new TmdbApiController();
+            var result = controller.FindMovies() as ViewResult;
+            Assert.AreEqual("FindMovies", result.ViewName);
+        }
+
+        //------- Test Find stars-------
+        [TestMethod()]
+        public void FindStars()
+        {
+            var controller = new TmdbApiController();
+            var result = controller.FindStars() as ViewResult;
+            Assert.AreEqual("FindStars", result.ViewName);
+        }
+
+        //------- Test Find similar movies-------
+        [TestMethod()]
+        public void FindSimilarMovies()
+        {
+            var controller = new TmdbApiController();
+            var result = controller.FindSimilarMovies() as ViewResult;
+            Assert.AreEqual("FindSimilarMovies", result.ViewName);
+        }
+
+        //------- Test Find Movies credits-------
+        [TestMethod()]
+        public void FindMovieCredits()
+        {
+            var controller = new TmdbApiController();
+            var result = controller.FindMovieCredits() as ViewResult;
+            Assert.AreEqual("FindMovieCredits", result.ViewName);
+        }
+
+
+
+    }
+}
